fd28facb195504516d7bec064ab6fe4ea4035c8f
Your Name <you@example.com>
2022-10-19 17:38:36 +0530

    Java Fundamentals

---
+++ src/com/bridgelabz/CompareTwoStrings.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class CompareTwoStrings {
+    public static void main(String[] args) {
+        String a = "Welcome to java";
+        String b = "Welcome to World";
+        System.out.println(a.equals(b));
+    }
+}
---
+++ src/com/bridgelabz/DataTypes.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+public class DataTypes {
+    static int a;
+    static long c;
+    static short d;
+    static byte e;
+    static boolean f;
+    static char g;
+    static float h;
+    static double i;
+    public static void main(String[] args) {
+        System.out.println("value of int: " + a);
+        // System.out.println("value of decimal: " + b);
+        System.out.println("value of long: " + c);
+        System.out.println("value of short: " + d);
+        System.out.println("value of byte: " + e);
+        System.out.println("value of boolean: " + f);
+        System.out.println("value of char: " + g);
+        System.out.println("value of float: " + h);
+        System.out.println("value of double: " + i);
+    }
+}
